b152b60a96fad6bfe57bc163639a5f675f039d6d
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-02-24 16:08:54 -0500

    Initial 1.14 project

---
+++ src/webparts/secureScript7/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ISecureScript7WebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'SecureScript7WebPartStrings' {
+  const strings: ISecureScript7WebPartStrings;
+  export = strings;
+}
