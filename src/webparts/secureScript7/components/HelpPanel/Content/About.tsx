baeb349a6e0b66017c7a9913595d30b4d37afeb7
mikezimm <49648086+mikezimm@users.noreply.github.com>
2022-03-07 19:57:47 -0500

    close #21 , bump version to 1.0.1.06

--- src/webparts/secureScript7/components/HelpPanel/Content/About.tsx
+++ src/webparts/secureScript7/components/HelpPanel/Content/About.tsx
@@ -19,6 +19,7 @@ export function aboutTable( repoLinks: IRepoLinks, showRepoLinks: boolean ) {
         rows: [],
     };
 
+    table.rows.push( createAboutRow('2022-03-08',"1.0.1.06","#27, #21 - Add File Type security", showRepoLinks === true ? repoLinks : null ) );
     table.rows.push( createAboutRow('2022-03-05',"1.0.1.05","Close #3, #22, #10 - add importProps functionality", showRepoLinks === true ? repoLinks : null ) );
     table.rows.push( createAboutRow('"',"\"","#15 - whitelist CDNs, #19 - quick peek tags", showRepoLinks === true ? repoLinks : null ) );
     table.rows.push( createAboutRow('"',"\"","#24 - Add Terms of Use", showRepoLinks === true ? repoLinks : null ) );
